c5557bb63617317fcaa3fe0992a64949254a02fa
molo32 <55426197+molo32@users.noreply.github.com>
2020-05-21 01:36:56 +0200

    Update class_example.py

--- class_example.py
+++ class_example.py
@@ -8,7 +8,7 @@ class Employee:
       Employee.empCount += 1
    
    def displayCount(self):
-     print "Total Employee %d" % Employee.empCount
+     print ("Total Employee %d" % Employee.empCount)
 
    def displayEmployee(self):
-      print "Name : ", self.name,  ", Salary: ", self.salary
+      print ("Name : ", self.name,  ", Salary: ", self.salary)
